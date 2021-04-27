1655930ff3fad196d5221b5ac144991612aedb78
StevenWuTG <stevenwu91@icloud.com>
2021-04-27 13:45:53 -0400

    triplex CLI practice done

--- TripleX/triplex.cpp
+++ TripleX/triplex.cpp
@@ -5,18 +5,45 @@ int main()
     std::cout << std::endl;
     std::cout << "You are FatGruber and you need to break the code to difuse the bomb!";
     std::cout << std::endl;
-    std::cout << "Enter the correct code to continue...";
+    std::cout << "Enter the correct code to continue...\n\n";
 
-    const int a = 4;
-    const int b = 3;
-    const int c = 2;
+    // Declare 3 numbers
+    const int CodeA = 4;
+    const int CodeB = 3;
+    const int CodeC = 2;
 
-    a = 10;
 
-    int abc = a * b * c;
+    const int CodeSum = CodeA + CodeB + CodeC;
+    const int CodeProduct = CodeA * CodeB * CodeC;
 
+    // print game variables 
     std::cout << std::endl;
-    std::cout << abc;
+    std::cout << "+ There are 3 numbers in the code\n";
+    std::cout << "\n+ The codes add up to : " << CodeSum;
+    std::cout << "\n+ The codes multiply to give : " << CodeProduct << std::endl;
+
+    //store player guess
+    int GuessA, GuessB, GuessC;
+    std::cin >> GuessA;
+    std::cin >> GuessB;
+    std::cin >> GuessC;
+    
+    std::cout << "\nYou entered:\n" << GuessA << GuessB << GuessC;
+
+    //game logic check
+    int GuessSum = GuessA + GuessB + GuessC;
+    int GuessProduct = GuessA * GuessB * GuessC;
+
+    if (GuessSum == CodeSum && GuessProduct == CodeProduct )
+    {
+        std::cout << "\nCongrats! You are the Weener!";
+    }
+    else
+    {
+        std::cout << "\nYou lose!" << "\nFATALITY!!!";
+    }
+
+
 
     return 0;
 }
